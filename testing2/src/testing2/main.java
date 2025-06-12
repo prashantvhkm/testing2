98ecf9d2860806c0a6abdee156c0ff3f43ef0173
prashantvhkm <prashantvishwakarma2435@gmail.com>
2025-06-12 17:34:42 +0530

    2

--- testing2/src/testing2/main.java
+++ testing2/src/testing2/main.java
@@ -3,5 +3,14 @@ package testing2;
 public class main {
 public static void main(String[] args) {
 	System.err.println("hello testing 2");
+
+	System.err.println("hello testing 3");
+	System.err.println("hello testing 2");
+	System.err.println("hello testing 2");
+	System.err.println("hello testing 2");
+	System.err.println("hello testing 2");
+	System.err.println("hello testing 2");
+	System.err.println("hello testing 2");
+	System.err.println("hello testing 2");
 }
 }
